a1c1aeeb164c65fa61a78e03072a579650b41b85
Michael Otty <michael.otty@gmail.com>
2020-03-16 20:27:48 +0000

    Fixed bug where the function changed the value of the input inline

--- __init__.py
+++ __init__.py
@@ -4,11 +4,11 @@ import numpy as np
 
 def envelope(input_signal):
     baseline_shift = np.mean(input_signal)
-    input_signal -= baseline_shift
+    normalised_signal = input_signal - baseline_shift
 
-    analytic_signal = hilbert(input_signal)
-    yupper = np.abs(analytic_signal)
-    ylower = -np.abs(analytic_signal)
+    analytic_signal = hilbert(normalised_signal)
+    yupper = np.abs(analytic_signal) + baseline_shift
+    ylower = -np.abs(-analytic_signal) + baseline_shift
 
     return (yupper, ylower)
 
