1bc5556ebce0b04d050e1ee5ad6deb8a662752ca
Michael Otty <michael.otty@gmail.com>
2020-03-16 20:24:01 +0000

    Added basic logic to envelope function

--- __init__.py
+++ __init__.py
@@ -1,8 +1,14 @@
 from scipy.signal import hilbert
+import numpy as np
+
 
 def envelope(input_signal):
-    yupper = None
-    ylower = None
+    baseline_shift = np.mean(input_signal)
+    input_signal -= baseline_shift
+
+    analytic_signal = hilbert(input_signal)
+    yupper = np.abs(analytic_signal)
+    ylower = -np.abs(analytic_signal)
 
     return (yupper, ylower)
 
